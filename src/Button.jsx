f80f993a8c2cd8f6a67e8b8aa747ad90c9f80cfa
cheddaryeon <cheddaryeon@gmail.com>
2023-06-23 13:01:08 +0900

    error

---
+++ src/Button.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import LargeBtn from "./components/Styled";
+
+function Button() {
+  const LPBtnHandler = () => {
+    alert("버튼을 만들어 보세요");
+  };
+
+  return (
+    <div>
+      <h1>Button</h1>
+      <div className="PrimaryBtn">
+        <LargeBtn onClick={LPBtnHandler}>Large Primary Button</LargeBtn>
+      </div>
+      <div className="NegativeBtn"></div>
+    </div>
+  );
+}
+
+export default Button;
---
+++ src/components/Styled.jsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const LargeBtn = styled.button`
+  height: 50px;
+  width: 300px;
+`;
